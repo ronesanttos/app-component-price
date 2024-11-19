08610d4291381a89cfe325916f8488059fe96e20
ronesanttos <devronesantos@gmail>
2024-11-18 22:01:10 -0300

    css quase finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
+        <div className="title">
+          <h1>Simple, traffic-based pricing</h1>
+          <p>Sign-up for our 30-day trial. No credit card required.</p>
+        </div>
+        <div className="card">
+          <div className="pageview">
+            <p>100K PAGEVIEWS</p>
+            <div className="price">
+              <span>$16.00</span>
+              <p>/month</p>
+            </div>
+          </div>
+        
+          <div className="billing">
+              <p>Monthly Billing</p>
+              <input type="radio" name="billing" id="billing" value={"true"} />
+              <p>Yearly Billing</p>
+              <span>25% discount</span>
+          </div>
+          <div className="linha">
+          </div>
+          <div className="info">
+            <ul>
+              <li>
+                <img src="/images/icon-check.svg" alt="check" /> Unlimited
+                websites
+              </li>
+              <li>
+                {" "}
+                <img src="/images/icon-check.svg" alt="check" />
+                100% data ownership
+              </li>
+              <li>
+                {" "}
+                <img src="/images/icon-check.svg" alt="check" />
+                Email reports
+              </li>
+            </ul>
+            <button>Start my trial</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
